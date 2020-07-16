57a3f3ef37d556607dd3ca570ee1db0acf42b310
Yacinesaf <yacinesaf96@gmail.com>
2020-07-16 16:10:00 -0400

    fixed search bar problem

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,19 +1,22 @@
 import React, { Component } from 'react'
-import { Grid, Tabs, Tab, Typography, InputBase, ClickAwayListener, Divider } from '@material-ui/core'
+import { Grid, Tabs, Tab, Typography, InputBase, Divider } from '@material-ui/core'
 import CloseOutlinedIcon from '@material-ui/icons/CloseOutlined';
 import SearchIcon from '@material-ui/icons/Search';
+import DateFnsUtils from '@date-io/date-fns';
+import {
+  KeyboardDatePicker,
+  MuiPickersUtilsProvider,
+} from '@material-ui/pickers';
 
 export default class SearchBar extends Component {
   constructor(props) {
     super(props)
-
     this.state = {
       value: 0,
       inputClicked: false,
-      checkinClicked: false,
-      checkoutClicked: false,
-      guestsClicked: false,
-      selectedDate: new Date()
+      checkinDate: null,
+      checkoutDate: null,
+
     }
   }
 
@@ -21,9 +24,11 @@ export default class SearchBar extends Component {
     this.setState({ value: newValue });
   };
 
-  changeDate = (date) => {
-    this.setState({ selectedDate: date });
+  checkinDate = () => {
+    this.refs.checkin.openDialog()
   }
+
+
 
   render() {
     return (
@@ -33,74 +38,55 @@ export default class SearchBar extends Component {
           <Tab style={{ textTransform: 'none', minWidth: 'initial' }} label="Experiences" />
           <Tab style={{ textTransform: 'none', minWidth: 'initial' }} label="Online experiences" />
         </Tabs>
-        <Grid container alignItems='center' style={{ borderRadius: 10, border: '1px solid lightgrey', marginTop: 20, padding: '3px 10px' }}>
-          <ClickAwayListener onClickAway={() => this.setState({ inputClicked: false })}>
-            <Grid item xs={4} onClick={() => this.setState({ inputClicked: true })} style={{
-              border: this.state.inputClicked ? '2px solid darkgrey' : '2px solid transparent',
-              borderRadius: this.state.inputClicked ? 10 : 0,
-              backgroundColor: this.state.inputClicked ? '#ededed' : 'transparent',
-              display: 'flex',
-              alignItems: 'center'
-            }}>
-              <div style={{ flexGrow: 1 }}>
-                <Typography variant='caption' style={{ fontWeight: 700 }}>LOCATION</Typography>
-                <InputBase
-                  onChange={(e) => {
-                    this.setState({ search: e.target.value });
-                  }}
-                  onKeyPress={this.handleKeyPress}
-                  style={{ width: '90%', padding: '0px !important' }}
-                  inputProps={{ placeholder: 'Where are you going?', type: 'text' }}
-                />
-              </div>
-              {this.state.inputClicked ? <CloseOutlinedIcon fontSize='small' /> : null}
-            </Grid>
-          </ClickAwayListener>
-          {this.state.inputClicked ? null : <Divider orientation='vertical' style={{ height: 70 }} />}
-          <ClickAwayListener onClickAway={() => this.setState({ checkinClicked: false })}>
-            <Grid item xs={2} onClick={() => this.setState({ checkinClicked: true })} style={{
-              border: this.state.checkinClicked ? '2px solid darkgrey' : '2px solid transparent',
-              borderRadius: this.state.checkinClicked ? 10 : 0,
-              backgroundColor: this.state.checkinClicked ? '#ededed' : 'transparent',
-              paddingLeft: 10
-            }}>
-              <Typography variant='caption' style={{ fontWeight: 700 }}>CHEK IN</Typography>
-              <Typography style={{ fontWeight: 300, color: 'lightgrey', fontSize: 12, paddingTop: 5 }}>Add dates</Typography>
-            </Grid>
-          </ClickAwayListener>
-          {this.state.checkinClicked ? null : <Divider orientation='vertical' style={{ height: 70 }} />}
-          <ClickAwayListener onClickAway={() => this.setState({ checkoutClicked: false })}>
-            <Grid item xs={2} onClick={() => this.setState({ checkoutClicked: true })} style={{
-              border: this.state.checkoutClicked ? '2px solid darkgrey' : '2px solid transparent',
-              borderRadius: this.state.checkoutClicked ? 10 : 0,
-              backgroundColor: this.state.checkoutClicked ? '#ededed' : 'transparent',
-              paddingLeft: 10
-            }}>
+        <Grid container alignItems='center' style={{ borderRadius: 10, border: '1px solid lightgrey', marginTop: 20 }}>
+          <Grid item xs={4} onClick={() => this.setState({ inputClicked: true })} style={{ display: 'flex', alignItems: 'center', padding: '0px 10px', height: '100%' }}>
+            <div style={{ flexGrow: 1 }}>
+              <Typography variant='caption' style={{ fontWeight: 700 }}>LOCATION</Typography>
+              <InputBase
+                onChange={(e) => {
+                  this.setState({ search: e.target.value });
+                }}
+                onKeyPress={this.handleKeyPress}
+                style={{ width: '90%', padding: '0px !important' }}
+                inputProps={{ placeholder: 'Where are you going?', type: 'text' }}
+              />
+            </div>
+            {this.state.inputClicked ? <CloseOutlinedIcon fontSize='small' /> : null}
+          </Grid>
+          <Divider orientation='vertical' style={{ height: 70 }} />
+          <Grid item xs={2} style={{ paddingLeft: 10 }}>
+            <Typography variant='caption' style={{ fontWeight: 700 }}>CHEK IN</Typography>
+            <Typography style={{ fontWeight: 300, color: 'lightgrey', fontSize: 12, paddingTop: 5 }}>{this.state.checkinDate ? this.state.checkinDate : 'Add dates'}</Typography>
+          </Grid>
+          <Divider orientation='vertical' style={{ height: 70 }} />
+          <Grid item xs={2} style={{ paddingLeft: 10 }}>
+            <Typography variant='caption' style={{ fontWeight: 700 }}>CHEK OUT</Typography>
+            <Typography style={{ fontWeight: 300, color: 'lightgrey', fontSize: 12, paddingTop: 5 }}>{this.state.checkoutDate ? this.state.checkoutDate : 'Add dates'}</Typography>
+          </Grid>
+          <Divider orientation='vertical' style={{ height: 70 }} />
+          <Grid item xs={3} style={{ paddingLeft: 10, display: 'flex', flexGrow: 1 }}>
+            <div>
               <Typography variant='caption' style={{ fontWeight: 700 }}>CHEK OUT</Typography>
-              <Typography style={{ fontWeight: 300, color: 'lightgrey', fontSize: 12, paddingTop: 5 }}>Add dates</Typography>
-            </Grid>
-          </ClickAwayListener>
-          {this.state.checkoutClicked ? null : <Divider orientation='vertical' style={{ height: 70 }} />}
-          <ClickAwayListener onClickAway={() => this.setState({ guestsClicked: false })}>
-            <Grid item xs={3} onClick={() => this.setState({ guestsClicked: true })} style={{
-              border: this.state.guestsClicked ? '2px solid darkgrey' : '2px solid transparent',
-              paddingLeft: 10,
-              borderRadius: this.state.guestsClicked ? 10 : 0,
-              backgroundColor: this.state.guestsClicked ? '#ededed' : 'transparent',
-              display: 'flex',
-              flexGrow: 1
-            }}>
-              <div>
-                <Typography variant='caption' style={{ fontWeight: 700 }}>CHEK OUT</Typography>
-                <Typography style={{ fontWeight: 300, color: 'lightgrey', fontSize: 12, paddingTop: 5 }}>Add guests</Typography>
-              </div>
-            </Grid>
-          </ClickAwayListener>
-          <div style={{ borderRadius: 5, backgroundColor: '#FF385C', color: 'white', textTransform: 'none', fontSize: 16, display: 'flex', alignItems: 'center', cursor: 'pointer', height: 'fit-content', padding: '13px 20px' }}>
+              <Typography style={{ fontWeight: 300, color: 'lightgrey', fontSize: 12, paddingTop: 5 }}>Add guests</Typography>
+            </div>
+          </Grid>
+          <div style={{ borderRadius: 5, backgroundColor: '#FF385C', color: 'white', textTransform: 'none', fontSize: 14, display: 'flex', alignItems: 'center', cursor: 'pointer', height: 'fit-content', padding: '10px 20px', justifyContent: 'center' }}>
             <SearchIcon fontSize='small' style={{ color: 'white', paddingRight: 5 }} />
           Search
         </div>
         </Grid>
+        <MuiPickersUtilsProvider ref='checkin' utils={DateFnsUtils}>
+          <KeyboardDatePicker
+            margin="normal"
+            id="date-picker-dialog"
+            label="Date picker dialog"
+            format="MM/dd/yyyy"
+            onChange={(e) => this.setState({ checkinDate: `${e.getDate()}/${e.getMonth() + 1}/${e.getFullYear()}` })}
+            KeyboardButtonProps={{
+              'aria-label': 'change date',
+            }}
+          />
+        </MuiPickersUtilsProvider>
       </div>
     )
   }
