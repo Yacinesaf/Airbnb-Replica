e95d3ecda1ece348a888e13a62fb3e8298ac6637
Yacinesaf <yacinesaf96@gmail.com>
2020-07-27 15:59:23 -0400

    landing page mobile is done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,9 +4,16 @@ import LanguageIcon from '@material-ui/icons/Language';
 import FacebookIcon from '@material-ui/icons/Facebook';
 import InstagramIcon from '@material-ui/icons/Instagram';
 import TwitterIcon from '@material-ui/icons/Twitter';
+import { useTheme } from '@material-ui/core/styles';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 
 
 function Footer(props) {
+
+
+
+  const theme = useTheme();
+  const smDown = useMediaQuery(theme.breakpoints.down('sm'));
   const footerLinks = [
     {
       title: 'ABOUT',
@@ -51,31 +58,45 @@ function Footer(props) {
   ]
 
   return (
-    <Grid container justify='center' style={{ padding: '0px 10px', backgroundColor : '#F7F7F7', borderTop : '1px solid #DDDDDD' }}>
+    <Grid container justify='center' style={{ padding: '0px 10px', backgroundColor: '#F7F7F7', borderTop: '1px solid #DDDDDD' }}>
       <Grid item xs={10}>
-        <Grid container justify='space-between' style={{padding : '40px 0px'}}>
+        <Grid container justify='space-between' style={{ paddingTop: 40 }}>
           {footerLinks.map((x, i) => (
-            <Grid key={i} item x={2}>
-              <Typography variant='caption' style={{fontWeight : 600, paddingBottom : 15, display : 'block'}}>{x.title}</Typography>
+            <Grid key={i} item xs={12} md={2}>
+              <Typography variant='caption' style={{ fontWeight: 600, paddingBottom: 15, display: 'block' }}>{x.title}</Typography>
               {x.subtitles.map((y, i) => (
-                <Typography variant='caption' style={{ fontWeight: 400, display : 'block', paddingBottom : 10 }} key={i}>{y}</Typography>
+                <Typography variant='caption' style={{ fontWeight: 400, display: 'block', paddingBottom: 10 }} key={i}>{y}</Typography>
               ))}
+              {smDown ? <Divider variant='fullWidth' style={{ margin: '15px 0px' }} /> : null}
             </Grid>
           ))}
         </Grid>
-        <Grid item xs={12}>
-          <Divider variant='fullWidth' />
-          <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '20px 0px' }}>
-            <Typography variant='caption' style={{ fontWeight: 400 }}>© 2020 Airbnb, Inc. All rights reserved·Privacy·Terms·Sitemap</Typography>
-            <div style={{ display: 'flex', alignItems: 'center' }}>
-              <LanguageIcon fontSize='small' />
-              <Typography variant='caption' style={{ fontWeight: 400, textDecoration: 'underline', paddingRight: 20 }}>English(CA)</Typography>
-              <Typography variant='caption' style={{ fontWeight: 400, textDecoration: 'underline', paddingRight: 30 }}>$ CAD</Typography>
-              <FacebookIcon fontSize='small' />
-              <TwitterIcon fontSize='small' style={{ padding: '0px 10px' }} />
-              <InstagramIcon fontSize='small' />
+        <Grid item xs={12} style={{ paddingBottom: 40 }}>
+          {smDown ?
+            <div>
+              <div style={{display : 'flex', alignItems : 'center'}}>
+                <Typography variant='body1' style={{ fontWeight: 400, textDecoration: 'underline', paddingRight: 20 }}>English(CA)</Typography>
+                <Typography variant='body1' style={{ fontWeight: 400, textDecoration: 'underline', paddingRight: 30 }}>$ CAD</Typography>
+              </div>
+              <Typography variant='body1' style={{ fontWeight: 400, display: 'block', paddingTop: 10 }}>© 2020 Airbnb, Inc. All rights reserved·Privacy·Terms·Sitemap</Typography>
             </div>
-          </div>
+            :
+            <div>
+              <Divider variant='fullWidth' />
+              <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '20px 0px' }}>
+                <Typography variant='caption' style={{ fontWeight: 400 }}>© 2020 Airbnb, Inc. All rights reserved·Privacy·Terms·Sitemap</Typography>
+                <div style={{ display: 'flex', alignItems: 'center' }}>
+                  <LanguageIcon fontSize='small' />
+                  <Typography variant='caption' style={{ fontWeight: 400, textDecoration: 'underline', paddingRight: 20 }}>English(CA)</Typography>
+                  <Typography variant='caption' style={{ fontWeight: 400, textDecoration: 'underline', paddingRight: 30 }}>$ CAD</Typography>
+                  <FacebookIcon fontSize='small' />
+                  <TwitterIcon fontSize='small' style={{ padding: '0px 10px' }} />
+                  <InstagramIcon fontSize='small' />
+                </div>
+              </div>
+            </div>
+          }
+
         </Grid>
       </Grid>
     </Grid>
